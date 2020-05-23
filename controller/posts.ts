da7f492bd192d4ab68b6656265a32acd72ae665b
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-05-23 12:00:03 +0800

    Implement posts CRUD rest api function

--- controller/posts.ts
+++ controller/posts.ts
@@ -27,32 +27,115 @@ let posts = [
     }
 ];
 
-// route    GET api/posts
+// GET api/posts
 const getPosts = ({ response }: { response: any }) => {
     response.body = {
         success: true,
-        data: posts
+        posts
     }
 }
 
-// route    GET api/posts/:id
-const getPost = ({ response }: { response: any }) => {
-    
+// GET api/posts/:id
+const getPost = ({ params, response }: { params: { id: number }, response: any }) => {
+    let responseBody;
+    let status;
+    const post: Post | undefined = posts.find(post => post.id == params.id);
+    if (post) {
+        status = 200;
+        responseBody = {
+            success: true,
+            post
+        }
+    } else {
+        status = 404;
+        responseBody = {
+            message: 'Post not found'
+        }
+    }
+    response.status = status;
+    response.body = responseBody;
 }
 
-// route    POST api/posts
-const addPost = ({ response }: { response: any }) => {
-    
+// POST api/posts
+const addPost = async ({ request, response }: { request: any, response: any }) => {
+    let status;
+    let responseBody;
+    let lastId = posts.reduce(
+        (max, post) => (post.id > max ? post.id : max), posts[0].id
+    );
+    const requestBody = await request.body();
+    if (!request.hasBody) {
+        status = 400;
+        responseBody = {
+            message: 'Please add request body!'
+        }
+    } else {
+        const post: Post = requestBody.value;
+        post.id = lastId+1;
+        posts.push(post);
+        status = 201;
+        responseBody = {
+            success: true,
+            message: 'Post has been added!',
+            post
+        }
+    }
+    response.status = status;
+    response.body = responseBody;
 }
 
-// route    PUT api/posts/:id
-const updatePost = ({ response }: { response: any }) => {
-    
+// PUT api/posts/:id
+const updatePost = async ({ params, request, response }: { params: { id: number }, request: any, response: any }) => {
+    let status;
+    let responseBody;
+    const post: Post | undefined = posts.find(post => post.id == params.id);
+    if (post) {
+        const requestBody = await request.body();
+        if (!request.hasBody) {
+            status = 400;
+            responseBody = {
+                message: 'Please add request body!'
+            }
+        } else {
+            const postUpdate: { title?: string, body?: string, author?: string } = requestBody.value;
+            posts = posts.map(post => post.id == params.id ? {...post, ...postUpdate} : post);
+            status = 200;
+            responseBody = {
+                success: true,
+                posts 
+            }
+        }
+    } else {
+        status = 404;
+        responseBody = {
+            message: 'Post not found'
+        }
+    }
+    response.status = status;
+    response.body = responseBody;
 }
 
-// route    DELETE api/posts/:id
-const deletePost = ({ response }: { response: any }) => {
-    
+// DELETE api/posts/:id
+const deletePost = ({ params, response }: { params: { id: number }, response: any }) => {
+    let status;
+    let responseBody;
+    const post: Post | undefined = posts.find(post => post.id == params.id);
+    if (post) {
+        posts = posts.filter(post => post.id != params.id);
+        status = 200;
+        responseBody = {
+            success: true,
+            message: 'Post has been deleted!',
+            posts
+        }
+    } else {
+        status = 404;
+        responseBody = {
+            message: 'Post not found!'
+        }
+    }
+    response.status = status;
+    response.body = responseBody;
 }
 
 export { getPosts, getPost, addPost, updatePost, deletePost };
